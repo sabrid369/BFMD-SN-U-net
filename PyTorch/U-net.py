237362c8f7b0926e2434ed727f5ec54ad4cad444
Sabri D. (Dehar) <sabridehar4@gmail.com>
2023-05-12 19:28:13 +0200

    Update U-net.py

--- PyTorch/U-net.py
+++ PyTorch/U-net.py
@@ -1,20 +1,19 @@
 from block_feature_map_distortion import *
 from switchable_norm_2d import *
 from bfmd_sn_unet_blocks import *
-
 class UNet(nn.Module):
     def __init__(self, n_channels, n_classes):
         super(UNet, self).__init__()
         self.n_channels = n_channels
         self.n_classes = n_classes
-        self.inc = (DoubleConv_BFMD_SN_Unet(n_channels, 16,distortion_probabilty=0.1,feature_num=16,SN=False,Distortion=False))
-        self.down1 = (Down(16, 32,distortion_probabilty= 0.1,feature_num=32,SN=False,Distortion=False))
-        self.down2 = (Down(32, 64,distortion_probabilty=0.1,feature_num=64,SN=False,Distortion=False))
-        self.down3 = (Down(64, 128,distortion_probabilty=0.1,feature_num=128,SN=False,Distortion=False))
+        self.inc = (DoubleConv_BFMD_SN_Unet(n_channels, 16,SN=False,Distortion=False))
+        self.down1 = (Down(16, 32,SN=False,Distortion=False))
+        self.down2 = (Down(32, 64,SN=False,Distortion=False))
+        self.down3 = (Down(64, 128,SN=False,Distortion=False))
 
-        self.up1 = (Up(128, 64 ,distortion_probabilty=0.1,feature_num=64,SN=False,Distortion=False))
-        self.up2 = (Up(64, 32,distortion_probabilty=0.1,feature_num=32,SN=False,Distortion=False))
-        self.up3 = (Up(32, 16,distortion_probabilty=0.1,feature_num=16,SN=False,Distortion=False))
+        self.up1 = (Up(128, 64,SN=False,Distortion=False))
+        self.up2 = (Up(64, 32,SN=False,Distortion=False))
+        self.up3 = (Up(32, 16,SN=False,Distortion=False))
         self.outc = (OutConv(16, n_classes))
 
     def forward(self, x):
